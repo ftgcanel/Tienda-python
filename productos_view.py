ef1301b03696ac943dceb5834f1a13b6e98d9af6
ftgcanel <fatimacanel2310@gmail.com>
2025-05-21 18:02:35 -0600

    Delete productos_view.py

--- productos_view.py
+++
@@ -1,6 +0,0 @@
-import tkinter as tk
-def cargar_productos(ventana):
-    productos_panel = tk.Frame(ventana, bg="gray", padx=0,pady=0,width=1000,height=500)
-    productos_panel.pack()
-
-    print("Panel productos cargado")
