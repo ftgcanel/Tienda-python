38fbd2e5dd518c977756dd7a6bb0bbb1a17e259c
ftgcanel <fatimacanel2310@gmail.com>
2025-08-06 12:06:00 -0600

    Delete paneles directory

--- paneles/header_view.py
+++
@@ -1,7 +0,0 @@
-import tkinter as tk
-
-def cargar_header(ventana):
-    header_panel = tk.Frame(ventana, bg="lightgray", padx=0,pady=0,width=1000,height=100)
-    header_panel.pack()
-
-    print("Panel header cargado")
